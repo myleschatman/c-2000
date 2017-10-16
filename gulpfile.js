f1e08089f4f426daf5e727e17a11ed8c1605536f
Myles Chatman <myleschatman@gmail.com>
2017-10-16 00:38:25 -0700

    Change min.js to .js for isoArcade enable body bug

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,8 @@ gulp.task('static', () => {
 
 gulp.task('libs', () => {
   return gulp.src(['./node_modules/phaser/build/phaser.min.js',
-    './node_modules/phaser-plugin-isometric/dist/phaser-plugin-isometric.min.js'
+    './node_modules/phaser-plugin-isometric/dist/phaser-plugin-isometric.js',
+    './node_modules/socket.io-client/socket.io.min.js'
   ])
     .pipe(gulp.dest('./build/scripts'));
 });
