b1bb0c090fb3cdafeb7ad83fc60dcdd939fe73ad
Myles Chatman <myleschatman@gmail.com>
2017-10-17 01:03:12 -0700

    Remove boundary collisions

--- src/client/states/boot.js
+++ src/client/states/boot.js
@@ -9,11 +9,11 @@ export default class Boot extends Phaser.State {
     this.game.time.advancedTiming = true;
 
     this.game.plugins.add(new Phaser.Plugin.Isometric(this.game));
-    this.game.world.setBounds(0, 0, 800, 600);
+    // this.game.world.setBounds(0, 0, 800, 400);
     this.game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
+    // this.game.iso.projectionAngle = 0.52;
 
-    this.game.iso.anchor.setTo(0.5, 0.5);
-    this.game.iso.projectionAngle = 0.52;
+    this.game.iso.anchor.setTo(0.5, 0.2);
   }
 
   create() {
