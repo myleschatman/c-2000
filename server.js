f4a58b1c5ed4d16e469ef497c193656be7b4c3b9
Myles Chatman <myleschatman@gmail.com>
2017-10-17 21:56:49 -0700

    Moved index.html to root, created server file, and providing eslint and bable config

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io').listen(server);
+
+// app.use('/src', express.static(__dirname + '/src'));
+// app.use('/static', express.static(__dirname + '/static'));
+app.use('/build', express.static(__dirname + '/build'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8081, function() {
+  console.log('Listening on ' + server.address().port);
+});
