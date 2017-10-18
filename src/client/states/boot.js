73030ee5e68f92e7fa5bb4eebc059156f3182150
Myles Chatman <myleschatman@gmail.com>
2017-10-17 20:11:53 -0700

    Remove world bounds and projection angle

--- src/client/states/boot.js
+++ src/client/states/boot.js
@@ -9,9 +9,7 @@ export default class Boot extends Phaser.State {
     this.game.time.advancedTiming = true;
 
     this.game.plugins.add(new Phaser.Plugin.Isometric(this.game));
-    // this.game.world.setBounds(0, 0, 800, 400);
     this.game.physics.startSystem(Phaser.Plugin.Isometric.ISOARCADE);
-    // this.game.iso.projectionAngle = 0.52;
 
     this.game.iso.anchor.setTo(0.5, 0.2);
   }
