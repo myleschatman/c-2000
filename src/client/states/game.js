2b3c93493cb4562d771dc85284c2424cb5e8795a
Myles Chatman <myleschatman@gmail.com>
2017-10-19 00:18:20 -0700

    Remove borders from tiles

--- src/client/states/game.js
+++ src/client/states/game.js
@@ -16,31 +16,15 @@ export default class Game extends Phaser.State {
   create() {
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    // this.grass = this.game.add.isoSprite(0, 64, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 128, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 192, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 256, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 320, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 384, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 448, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 512, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 576, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 640, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 704, 0, 'grass_large');
-    // // this.grass = this.game.add.isoSprite(0, 768, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 832, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(0, 66, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(0, 132, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(0, 198, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(0, 264, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(0, 330, 0, 'grass_large');
 
-    this.grass = this.game.add.isoSprite(0, 66, 0, 'grass_border');
-    this.grass = this.game.add.isoSprite(0, 132, 0, 'grass_border');
-    this.grass = this.game.add.isoSprite(0, 198, 0, 'grass_border');
-    this.grass = this.game.add.isoSprite(0, 264, 0, 'grass_border');
-    this.grass = this.game.add.isoSprite(0, 330, 0, 'grass_border');
-    // this.grass = this.game.add.isoSprite(0, 384, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 448, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 512, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 576, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 640, 0, 'grass_large');
-    // this.grass = this.game.add.isoSprite(0, 704, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(66, 66, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(132, 66, 0, 'grass_large');
+    this.grass = this.game.add.isoSprite(198, 66, 0, 'grass_large');
 
     this.player = new Player(this.game);
     this.game.add.existing(this.player);
@@ -49,4 +33,5 @@ export default class Game extends Phaser.State {
   update() {
 
   }
+
 }
