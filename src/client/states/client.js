39fdae6f104b43af0e3edb5381e580f2111e1eff
Myles Chatman <myleschatman@gmail.com>
2017-10-26 15:54:38 -0700

    Players are created from communication between client and server

--- src/client/states/client.js
+++ src/client/states/client.js
@@ -1,11 +1,20 @@
 import io from 'socket.io-client';
+import Player from '../prefabs/player';
 
 export default class Client {
   constructor() {
     this.socket = io.connect();
+
+    this.playerMap = {};
   }
 
-  askNewPlayer() {
-    this.socket.emit('newplayer');
+  // askNewPlayer() {
+  //   this.socket.emit('newplayer');
+  // }
+
+  addNewPlayer(game) {
+    this.socket.on('newplayer', function(data) {
+      this.playerMap[data.id] = new Player(game, data.x, data.y, data.z);
+    });
   }
 }
--- src/client/states/game.js
+++ src/client/states/game.js
@@ -14,28 +14,15 @@ export default class Game extends Phaser.State {
 
   }
 
-  create() {
+  create(game) {
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
-    this.grass = this.game.add.isoSprite(0, 66, 0, 'grass_large');
-    this.grass = this.game.add.isoSprite(0, 132, 0, 'grass_large');
-    this.grass = this.game.add.isoSprite(0, 198, 0, 'grass_large');
-    this.grass = this.game.add.isoSprite(0, 264, 0, 'grass_large');
-    this.grass = this.game.add.isoSprite(0, 330, 0, 'grass_large');
-
-    this.grass = this.game.add.isoSprite(66, 66, 0, 'grass_large');
-    this.grass = this.game.add.isoSprite(132, 66, 0, 'grass_large');
-    this.grass = this.game.add.isoSprite(198, 66, 0, 'grass_large');
-
-    this.player = new Player(this.game);
-    this.game.add.existing(this.player);
-
-    this.connection = new Client();
-    this.connection.askNewPlayer();
+    this.client = new Client();
+    // this.client.askNewPlayer();
+    this.client.addNewPlayer(this.game);
   }
 
   update() {
 
   }
-
 }
