15c4ca86d46250ca3c44c04b065ccc49fd1c2cfa
Myles Chatman <myleschatman@gmail.com>
2017-10-29 16:54:02 -0700

    destroy function not working

--- src/client/states/game.js
+++ src/client/states/game.js
@@ -19,16 +19,15 @@ export default class Game extends Phaser.State {
 
     this.cursors = this.game.input.keyboard.createCursorKeys();
     this.socket = io.connect('http://localhost:8081');
-
-    // this.sprite = this.game.add.sprite(0, 0, 'blueplayer');
-    // this.sprite.kill();
-    // console.log(this.sprite);
     // this.player = new Player(this.game, 0, 0, 0);
     // this.game.add.existing(this.player);
-    // this.player.kill();
+    this.player = this.game.add.isoSprite(0,0,0,'blueplayer',0);
+    this.game.physics.isoArcade.enable(this.player);
+    this.game.camera.follow(this.player);
+    this.player.destroy();
     // delete this.player;
     // console.log(this.player);
-    // this.setEventHandlers(this.game);
+    //this.setEventHandlers(this.game);
   }
 
   update() {
@@ -42,17 +41,21 @@ export default class Game extends Phaser.State {
 
       this.socket.on('newplayer', (data) => {
         this.playerMap[data.id] = new Player(game, data.x, data.y, data.z);
+        game.add.existing(this.playerMap[data.id]);
       });
 
       this.socket.on('allplayers', (data) => {
         for (var i = 0; i < data.length; i++) {
           this.playerMap[data[i].id] = new Player(game, data[i].x, data[i].y, data[i].z);
+          game.add.existing(this.playerMap[data[i].id]);
+          console.log(this.playerMap[data[i].id]);
+          this.playerMap[data[i].id].destroy();
         }
       });
 
       this.socket.on('remove', (id) => {
-        this.playerMap[id].destroy();
-        delete this.playerMap[id];
+        // this.playerMap[id].destroy();
+        // delete this.playerMap[id];
       });
 
       if (this.cursors.down.isDown) {
