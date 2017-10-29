5ac0b80272db3a3643939773325a2faf60fb3de5
Myles Chatman <myleschatman@gmail.com>
2017-10-29 11:33:56 -0700

    add functionality to remove sprite on disconnect

--- src/client/states/game.js
+++ src/client/states/game.js
@@ -20,7 +20,15 @@ export default class Game extends Phaser.State {
     this.cursors = this.game.input.keyboard.createCursorKeys();
     this.socket = io.connect('http://localhost:8081');
 
-    this.setEventHandlers(this.game);
+    // this.sprite = this.game.add.sprite(0, 0, 'blueplayer');
+    // this.sprite.kill();
+    // console.log(this.sprite);
+    // this.player = new Player(this.game, 0, 0, 0);
+    // this.game.add.existing(this.player);
+    // this.player.kill();
+    // delete this.player;
+    // console.log(this.player);
+    // this.setEventHandlers(this.game);
   }
 
   update() {
@@ -41,6 +49,12 @@ export default class Game extends Phaser.State {
           this.playerMap[data[i].id] = new Player(game, data[i].x, data[i].y, data[i].z);
         }
       });
+
+      this.socket.on('remove', (id) => {
+        this.playerMap[id].destroy();
+        delete this.playerMap[id];
+      });
+
       if (this.cursors.down.isDown) {
         this.movement = {
           x: 1,
