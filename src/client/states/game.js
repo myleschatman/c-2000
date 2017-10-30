a949a38a5cc96dba1ad2d6f334f6df3036c6a731
Myles Chatman <myleschatman@gmail.com>
2017-10-30 01:41:44 -0700

    move event listeners from server to client.js

--- src/client/states/game.js
+++ src/client/states/game.js
@@ -1,9 +1,10 @@
 import Player from '../prefabs/player';
-import io from 'socket.io-client';
+import Client from '../client';
 
 export default class Game extends Phaser.State {
   constructor() {
     super();
+    this.client = new Client(this);
   }
 
   init() {
@@ -15,40 +16,42 @@ export default class Game extends Phaser.State {
   }
 
   create() {
-    this.socket = io.connect('http://localhost:8081');
-
+    // this.setEventHandlers(this.game);
     this.playerMap = {};
 
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-
-    this.setEventHandlers(this.game);
+    this.client.getNewPlayer();
   }
 
   update() {
 
   }
 
-  setEventHandlers(game) {
-    this.socket.on('connect', () => {
+  // setEventHandlers(game) {
+  //   this.socket.on('connect', () => {
 
-      this.socket.emit('newplayer');
+  //     this.socket.emit('newplayer');
 
-      this.socket.on('newplayer', (data) => {
-        this.playerMap[data.id] = new Player(game, data.x, data.y, data.z);
-        game.add.existing(this.playerMap[data.id]);
-      });
+  //     this.socket.on('newplayer', (data) => {
+  //       this.playerMap[data.id] = new Player(game, data.x, data.y, data.z);
+  //       game.add.existing(this.playerMap[data.id]);
+  //     });
 
-      this.socket.on('allplayers', (data) => {
-        for (var i = 0; i < data.length; i++) {
-          this.playerMap[data[i].id] = new Player(game, data[i].x, data[i].y, data[i].z);
-          game.add.existing(this.playerMap[data[i].id]);
-        }
-      });
+  //     this.socket.on('allplayers', (data) => {
+  //       for (var i = 0; i < data.length; i++) {
+  //         this.playerMap[data[i].id] = new Player(game, data[i].x, data[i].y, data[i].z);
+  //         game.add.existing(this.playerMap[data[i].id]);
+  //       }
+  //     });
 
-      this.socket.on('remove', (id) => {
-        this.playerMap[id].destroy();
-        delete this.playerMap[id];
-      });
-    });
+  //     this.socket.on('remove', (id) => {
+  //       this.playerMap[id].destroy();
+  //       delete this.playerMap[id];
+  //     });
+  //   });
+  // }
+
+  addNewPlayer(id, x, y, z) {
+    this.playerMap[id] = new Player(this.game, x, y, z);
+    this.game.add.existing(this.playerMap[id]);
   }
 }
