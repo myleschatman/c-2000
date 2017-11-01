06edbac2c6a02f37300901f6bbefacb5475adbf0
Myles Chatman <myleschatman@gmail.com>
2017-10-31 21:25:22 -0700

    Positions players on screen and moves players

--- src/client/client.js
+++ src/client/client.js
@@ -4,13 +4,26 @@ export default class Client {
   constructor(game) {
     this.socket = io.connect('http://localhost:8081');
 
-    this.socket.on('newplayer', (data) => {
+    this.socket.on('new player', (data) => {
       game.addNewPlayer(data.id, data.x, data.y, data.z);
+    });
+
+    this.socket.on('all players', (data) => {
+      for (var i = 0; i < data.length; i++) {
+        game.addNewPlayer(data[i].id, data[i].x, data[i].y, data[i].z);
+      }
+    });
+
+    this.socket.on('move player', (data) => {
+      game.movePlayer(data.id, data.x, data.y);
     });
   }
 
   getNewPlayer() {
-    this.socket.emit('newplayer');
+    this.socket.emit('new player');
   }
 
+  sendMovement(speed) {
+    this.socket.emit('click', speed);
+  }
 }
